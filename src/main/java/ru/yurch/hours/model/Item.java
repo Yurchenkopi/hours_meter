dd511fcac1fce690d3a3d11b10526c536c38ffca
Pavel <heppie@yandex.ru>
2025-07-04 14:07:52 +0300

    ItemService: remove operation bug fix

--- src/main/java/ru/yurch/hours/model/Item.java
+++ src/main/java/ru/yurch/hours/model/Item.java
@@ -19,7 +19,7 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
